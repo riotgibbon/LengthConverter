3eeea728c54c0d8c6ddddc76985482672e431d08
Toby <tobysevans@gmail.com>
2014-05-29 14:16:46 +0100

    Added available units to Python Thrift client

--- Thrift/ThriftConvert.Python/convert.py
+++ Thrift/ThriftConvert.Python/convert.py
@@ -11,6 +11,8 @@ print("[Client] opened socket")
 protocol = TBinaryProtocol.TBinaryProtocol(socket) 
 
 client = ConvertSvc.Client(protocol)
+units = client.availableUnits_func()
+print("Available units: %s" % ', '.join(units))
 input = raw_input('Please enter length to convert: ')
 msg = client.convert_func(input);
 print("[Client] received: %s" % msg)
