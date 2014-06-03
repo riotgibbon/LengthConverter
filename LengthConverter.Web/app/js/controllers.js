da4ad88ecb220c0d637dd44f5e46d7c437200b4a
Toby <tobysevans@gmail.com>
2014-06-03 15:08:08 +0100

    Dynamically loading units from service

--- LengthConverter.Web/app/js/controllers.js
+++ LengthConverter.Web/app/js/controllers.js
@@ -1,9 +1,13 @@
 ﻿(function () {
     var app = angular.module("converter", []);
-    app.controller("ConverterController", function() {
+    app.controller("ConverterController", ['$http', function ($http) {
+        var converter = this;
         this.units = ["cm", "m"];
+        $http.get('/Converter/Formats').success(function(data) {
+            converter.units = data;
+        });
         this.length = 1;
         this.inputFormat = this.units[0];
         this.outputFormat = this.units[0];
-    });
+    }]);
 })();
