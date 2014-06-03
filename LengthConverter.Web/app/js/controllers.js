0b933afb3fd5711d359bce7b597681e7ca0f18de
Toby <tobysevans@gmail.com>
2014-06-03 14:59:03 +0100

    Added basic angular model to front page

---
+++ LengthConverter.Web/app/js/controllers.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var app = angular.module("converter", []);
+    app.controller("ConverterController", function() {
+        this.units = ["cm", "m"];
+        this.length = 1;
+        this.inputFormat = this.units[0];
+        this.outputFormat = this.units[0];
+    });
+})();
